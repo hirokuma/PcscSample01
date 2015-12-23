a3d60c86b5b0e2bfeeb7dea4b5eb7aa2d3635ac8
hirokuma <hiro9ma@gmail.com>
2015-12-23 14:38:20 +0900

    Get Data追加

---
+++ PcscSample01/PcscSample01.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace PcscSample01
+{
+    partial class PcscSample01
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRwDetect = new System.Windows.Forms.Button();
+            this.comboRw = new System.Windows.Forms.ComboBox();
+            this.buttonStatus = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.groupControl = new System.Windows.Forms.GroupBox();
+            this.buttonGetData = new System.Windows.Forms.Button();
+            this.groupControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonRwDetect
+            // 
+            this.buttonRwDetect.Location = new System.Drawing.Point(295, 10);
+            this.buttonRwDetect.Name = "buttonRwDetect";
+            this.buttonRwDetect.Size = new System.Drawing.Size(75, 23);
+            this.buttonRwDetect.TabIndex = 0;
+            this.buttonRwDetect.Text = "R/W detect";
+            this.buttonRwDetect.UseVisualStyleBackColor = true;
+            this.buttonRwDetect.Click += new System.EventHandler(this.buttonRwDetect_Click);
+            // 
+            // comboRw
+            // 
+            this.comboRw.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboRw.FormattingEnabled = true;
+            this.comboRw.Location = new System.Drawing.Point(12, 12);
+            this.comboRw.Name = "comboRw";
+            this.comboRw.Size = new System.Drawing.Size(277, 20);
+            this.comboRw.TabIndex = 1;
+            // 
+            // buttonStatus
+            // 
+            this.buttonStatus.Enabled = false;
+            this.buttonStatus.Location = new System.Drawing.Point(6, 18);
+            this.buttonStatus.Name = "buttonStatus";
+            this.buttonStatus.Size = new System.Drawing.Size(75, 23);
+            this.buttonStatus.TabIndex = 2;
+            this.buttonStatus.Text = "Status";
+            this.buttonStatus.UseVisualStyleBackColor = true;
+            this.buttonStatus.Click += new System.EventHandler(this.buttonStatus_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Enabled = false;
+            this.buttonConnect.Location = new System.Drawing.Point(12, 38);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(358, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // groupControl
+            // 
+            this.groupControl.Controls.Add(this.buttonGetData);
+            this.groupControl.Controls.Add(this.buttonStatus);
+            this.groupControl.Location = new System.Drawing.Point(12, 67);
+            this.groupControl.Name = "groupControl";
+            this.groupControl.Size = new System.Drawing.Size(358, 194);
+            this.groupControl.TabIndex = 4;
+            this.groupControl.TabStop = false;
+            this.groupControl.Text = "Card Control";
+            // 
+            // buttonGetData
+            // 
+            this.buttonGetData.Location = new System.Drawing.Point(87, 18);
+            this.buttonGetData.Name = "buttonGetData";
+            this.buttonGetData.Size = new System.Drawing.Size(75, 23);
+            this.buttonGetData.TabIndex = 3;
+            this.buttonGetData.Text = "GetData";
+            this.buttonGetData.UseVisualStyleBackColor = true;
+            this.buttonGetData.Click += new System.EventHandler(this.buttonGetData_Click);
+            // 
+            // PcscSample01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(382, 273);
+            this.Controls.Add(this.groupControl);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboRw);
+            this.Controls.Add(this.buttonRwDetect);
+            this.Name = "PcscSample01";
+            this.Text = "PC/SC Sample01";
+            this.groupControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRwDetect;
+        private System.Windows.Forms.ComboBox comboRw;
+        private System.Windows.Forms.Button buttonStatus;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.GroupBox groupControl;
+        private System.Windows.Forms.Button buttonGetData;
+    }
+}
+
---
+++ PcscSample01/PcscSample01.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//https://github.com/danm-de/pcsc-sharp
+//https://danm.de/docs/pcsc-sharp/index.html
+using PCSC;
+
+namespace PcscSample01
+{
+    public partial class PcscSample01 : Form
+    {
+        SCardContext mContext;
+        SCardReader mReader;
+
+        public PcscSample01()
+        {
+            InitializeComponent();
+
+            //リソースマネージャとの接続
+            mContext = new SCardContext();
+            mContext.Establish(SCardScope.User);
+            mReader = new SCardReader(mContext);
+
+            int num = updateComboRw();
+            buttonConnect.Enabled = (num != 0);
+        }
+
+        private void buttonRwDetect_Click(object sender, EventArgs e)
+        {
+            int num = updateComboRw();
+            buttonConnect.Enabled = (num != 0);
+            if (num == 0)
+            {
+                MessageBox.Show("R/W not found", "NOT FOUND", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            if (!mReader.IsConnected)
+            {
+                //接続
+                SCardError err = mReader.Connect((string)comboRw.SelectedItem, SCardShareMode.Shared, SCardProtocol.Any);
+                if (err == SCardError.Success)
+                {
+                    buttonConnect.Text = "Disconnect";
+                    buttonRwDetect.Enabled = false;
+                    comboRw.Enabled = false;
+
+                    foreach (Control ctrl in groupControl.Controls)
+                    {
+                        ctrl.Enabled = true;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Fail connect.");
+                }
+            }
+            else
+            {
+                //切断
+                SCardError err = mReader.Disconnect(SCardReaderDisposition.Leave);
+                if (err == SCardError.Success)
+                {
+                    buttonConnect.Text = "Connect";
+                    buttonRwDetect.Enabled = true;
+                    comboRw.Enabled = true;
+
+                    foreach (Control ctrl in groupControl.Controls)
+                    {
+                        ctrl.Enabled = false;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Fail disconnect.");
+                }
+            }
+        }
+
+        private void buttonStatus_Click(object sender, EventArgs e)
+        {
+            displayReaderStatus(mReader);
+        }
+
+
+        private void buttonGetData_Click(object sender, EventArgs e)
+        {
+            sendGetData(mReader);
+        }
+
+        ///////////////////////////////////////////////////////////////////////////////////
+
+        private int updateComboRw()
+        {
+            comboRw.Items.Clear();
+            string[] readerNames = mContext.GetReaders();
+            if ((readerNames == null) || (readerNames.Length == 0))
+            {
+                return 0;
+            }
+
+            foreach (string reader in readerNames)
+            {
+                comboRw.Items.Add(reader);
+            }
+            comboRw.SelectedIndex = 0;
+
+            return readerNames.Length;
+        }
+
+        private void displayReaderStatus(ISCardReader reader)
+        {
+            string[] readerNames;
+            SCardProtocol proto;
+            SCardState state;
+            byte[] atr;
+            SCardError err;
+
+            err = reader.Status(out readerNames, out state, out proto, out atr);
+            if (err == SCardError.Success)
+            {
+                MessageBox.Show("Reader : " + readerNames[0] + "\nProtocol : " + proto + "\nState : " + state + "\nATR : " + BitConverter.ToString(atr));
+            }
+            else
+            {
+                MessageBox.Show("Fail Status.");
+            }
+        }
+
+        private void sendGetData(ISCardReader reader)
+        {
+            SCardError err;
+
+            err = reader.BeginTransaction();
+            if (err != SCardError.Success)
+            {
+                MessageBox.Show("Fail BeginTransaction.");
+                return;
+            }
+
+            // pcsc3_v2.01.09.pdf
+            // p.31
+            // "3.2.2.1.3  Get Data Command"
+            byte[] cmd = { 0xff, 0xca, 0x00, 0x00, 0x00 };
+            byte[] res = new byte[10];
+            err = reader.Transmit(cmd, ref res);
+
+            reader.EndTransaction(SCardReaderDisposition.Leave);
+
+            if (err == SCardError.Success)
+            {
+                MessageBox.Show("result : " + BitConverter.ToString(res, 0, res.Length - 2) +
+                    "\nSW1 : " + res[res.Length - 2].ToString("x2") + 
+                    "\nSW2 : " + res[res.Length - 1].ToString("x2"));
+            }
+            else
+            {
+                MessageBox.Show("Fail Transmit");
+            }
+        }
+    }
+}
